199776d4692affba9da4c0bcf649f43f606f28b1
ravi-cloud511 <ravi.cloud8700@gmail.com>
2026-05-23 19:01:26 +0530

    Create src/main/java/com/horizontechx/studentapp/Student.java

---
+++ src/main/java/com/horizontechx/studentapp/Student.java
@@ -0,0 +1,22 @@
+package com.horizontechx.studentapp;
+
+public class Student {
+    private Long id;
+    private String name;
+    private String course;
+
+    public Student() {}
+    
+    public Student(Long id, String name, String course) {
+        this.id = id;
+        this.name = name;
+        this.course = course;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getCourse() { return course; }
+    public void setCourse(String course) { this.course = course; }
+}
